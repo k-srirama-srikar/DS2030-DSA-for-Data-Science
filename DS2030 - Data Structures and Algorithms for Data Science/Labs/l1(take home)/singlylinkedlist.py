3ce4edfc883dee9261d4457b4db26127c0e91484
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-08-11 23:14:20 +0530

    Update singlylinkedlist.py

--- DS2030 - Data Structures and Algorithms for Data Science/Labs/l1(take home)/singlylinkedlist.py
+++ DS2030 - Data Structures and Algorithms for Data Science/Labs/l1(take home)/singlylinkedlist.py
@@ -5,26 +5,33 @@ class Node:
     def __init__(self, e, n):
         self.element = e
         self.next = n
+        # stores the element and the address to the next node (or None if it is the ending)
  
     def getElement(self):
+        # function to return the element
         return self.element
 
     def getNext(self):
+        # gets the next element
         return self.next
     
     def setNext(self, iNode):
+        # sets a node as the next node
         self.next = iNode
 
 
 class SLLIterator:
     # making an iterator for the singly liniked lists so that list traversal becomes very easy
+    #we can use for-each to easily loop though the linked list
     def __init__(self, head):
+        # we pass the head of a SinglyLinkedList to the SLLIterator class
         self.curr = head
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        # we define the process of the iteration here
         if not self.curr:
             raise StopIteration
         else:
@@ -40,17 +47,23 @@ class SinglyLinkedList:
         self.tail = Node(None, None)
         self.head.setNext(self.tail)
         self._size = 0
+        # I've made the tail node just in case.. As I was not very clear on solution
 
     def __len__(self):
         return self._size
+        # we can just type len(the singly linked list object) and get its size
 
     def __iter__(self):
+        # the iterator here redirects to the SLLIterator class
         return SLLIterator(self.head)
 
     def isEmpty(self):
+        # a function to check if the list is empty or not
         return self._size == 0
 
     def addFirst(self, elem_val):
+        # this can be used to add the element at the beginning of the linkedlist
+        # also a point to note is that I haven't declared the head to contain a None element, rather I've set the first element as the head
         self.head = Node(elem_val, self.head)
         if self._size == 0:
             self.tail = self.head
@@ -58,6 +71,7 @@ class SinglyLinkedList:
         self._size+=1
     
     def addLast(self, elem_val):
+        # this function adds the element at the end of the array
         iNode = Node(elem_val, None)
         if (self.isEmpty()):
             self.head = iNode
@@ -67,10 +81,12 @@ class SinglyLinkedList:
         self._size += 1
 
     def add(self, elem_val):
+        # this alson adds the function at the end... I just made it to keep the function name shorter
         self.addLast(elem_val)
 
     def pop(self):
         # I was unable to think of a better way than this to pop the last elelement for a singly linked list, so... I just went with it...
+        # although it loops through the list everytime to pop the last element, it rather made the logic for the task pretty easy
         if self._size == 0:
             return None
         elif self._size == 1:
@@ -83,4 +99,3 @@ class SinglyLinkedList:
                 self.tail = e
                 e.next = None
                 return temp
-
