7c7f080f7ff9db54dd0be4b035c6a9c09c5e26c5
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-08-11 23:13:18 +0530

    Update Task_1.py

--- DS2030 - Data Structures and Algorithms for Data Science/Labs/l1(take home)/Task_1.py
+++ DS2030 - Data Structures and Algorithms for Data Science/Labs/l1(take home)/Task_1.py
@@ -1,13 +1,17 @@
 from singlylinkedlist import Node, SinglyLinkedList
 x = input().split()
-
+# storing the input vbalues in a list initially
 sl = SinglyLinkedList()
 
 for i in x:
     sl.add(i)
 
+# adding the elements of the list to the singly linked list object
+
 def rearrange(sl_obj):
+    # the function to rearrange the elements of the linked list
     new_sl = SinglyLinkedList()
+    # an empty list to which we append everything
     check_n = len(sl_obj)//2
     if len(sl_obj) == 2*check_n:
         check_n-=1
@@ -15,11 +19,13 @@ def rearrange(sl_obj):
     for node in sl_obj:       
         n1 = node.getElement()
         new_sl.add(n1)
+        # we add the current element and the last element and remove the last element to make the list shorter
         node2 = sl_obj.pop()
         n2 = node2.getElement()
         if check_n >0:
             new_sl.add(n2)
             check_n -= 1  
+            # just to make sure that the last element is nott added
     return new_sl
 
 sl = rearrange(sl)
@@ -28,4 +34,3 @@ for e in sl:
     print(e.getElement(), end=" ")
     if e.next!=None:
         print("->", end=" ")
-
