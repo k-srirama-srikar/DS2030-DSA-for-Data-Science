b2a3b1cf7b9472187611e95b4f9dbbe8ff44c6eb
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-08-12 13:53:15 +0530

    Update Task_2.py

--- DS2030 - Data Structures and Algorithms for Data Science/Labs/l1(take home)/Task_2.py
+++ DS2030 - Data Structures and Algorithms for Data Science/Labs/l1(take home)/Task_2.py
@@ -1,20 +1,26 @@
+from singlylinkedlist import SinglyLinkedList
+
 number = int(input())
-bookmark_set = set()
+bookmark_set = SinglyLinkedList()
+# a singlylinkedlist to keep track of the results that we have gotten and if it is in it, that implies that the number is not happy
 s1 = 0
-# is_happy = False if number!=1 else True
 sum1 = 0
 def recurr_sum(n):
+    # a recursive function to add the numbers in the number
     if n<10:
         return n**2
     else:
         s = recurr_sum(n//10) + (n%10)**2
         return s
 s_init = recurr_sum(number)
+# the initial sum
 is_happy = True if s_init == 1 else False
 bookmark_set.add(s_init)
+# adding it to the bookmark set
 sum1 = s_init
 while sum1 !=1:
     sum1 = recurr_sum(sum1)
+    # to check all the sums that come after the initial
     if sum1 == 1:
         is_happy = True
         break
@@ -24,5 +30,4 @@ while sum1 !=1:
         break
 
 print(is_happy)
-
 
